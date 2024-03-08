7f8b5d43353f59656cd5581288552d1b57ea6906
Eduardo Viana Dantas da Costa <eduardo.viana@foco.local>
2024-03-08 10:44:26 -0300

    botão de sair adicionado

--- library.front-end/src/app/app.component.ts
+++ library.front-end/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { LoginVerifiedService } from './shared/services/login-verified.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,18 @@ import { LoginVerifiedService } from './shared/services/login-verified.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(public loginVerifiedService: LoginVerifiedService) {}
+  constructor(
+    public loginVerifiedService: LoginVerifiedService,
+    private router: Router
+  ) {}
   ngOnInit(): void {}
   ngOnDestroy(): void {}
+
+  logOff() {
+    alert('Você saiu da sessão!');
+    setTimeout(() => {
+      this.loginVerifiedService.toggleVerifiedUser();
+      this.router.navigate(['/login']);
+    }, 500);
+  }
 }
